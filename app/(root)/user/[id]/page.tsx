87929745badef399acb413a7f644db6e67117e26
aaronersando <agersando@gmail.com>
2025-10-22 12:38:09 +0800

    fix: correct className formatting for user name display

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -21,7 +21,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
       <section className="w-full pb-10 pt-20 px-6 max-w-7xl mx-auto lg:flex-row flex-col flex gap-10">
         <div className="w-80 px-6 pb-6 pt-20 flex flex-col justify-center items-center relative z-0 h-fit rounded-[30px] max-lg:w-full bg-[var(--bg-800)] border-[5px] border-[var(--border)] shadow-[0_6px_18px_rgba(2,6,10,0.6)]">
           <div className="w-11/12 bg-[var(--txt-primary)] border-[5px] border-[var(--border)] rounded-[20px] px-5 py-3 absolute -top-9 flex items-center justify-center z-10 shadow-[2px_2px_0_rgba(0,0,0,0.6)] text-[color:var(--bg-900)]">
-            <h3 className="text-24-black uppercase text-center line-clamp-1">
+            <h3 className="text-[24px] font-black text-[color:var(--bg-900)] uppercase text-center line-clamp-1">
               {user.name}
             </h3>
           </div>
